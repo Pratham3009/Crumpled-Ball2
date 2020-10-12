77f53d7531c4d2c76ff37c832562d76e9c6cc26d
Pratham3009 <70432855+Pratham3009@users.noreply.github.com>
2020-10-12 14:44:40 +0530

    Add files via upload

---
+++ dustbin.js
@@ -0,0 +1,17 @@
+class dustbinClass{
+    constructor(x,y){
+        var options = {
+            isStatic:false,
+            resitution:0.3,
+            friction:0.5,
+            density:1.2
+        }
+        
+           dustbin = loadImage("dustbingreen.png");
+
+           display()
+            image(this.image, 0, 0);
+
+           
+    }
+}
